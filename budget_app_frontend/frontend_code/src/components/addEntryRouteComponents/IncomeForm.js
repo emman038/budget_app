cb8d77e52cf80870cc9192cd1b9164f817cc496a
Emmanuel Ajayi <emmanuelajayi380@yahoo.co.uk>
2024-01-04 00:18:30 +0000

    Created function to handle change in income form to update state

--- budget_app_frontend/frontend_code/src/components/addEntryRouteComponents/IncomeForm.js
+++ budget_app_frontend/frontend_code/src/components/addEntryRouteComponents/IncomeForm.js
@@ -1,16 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const IncomeForm = ({listOfIncomeSources}) => {
+
+    const navigate = useNavigate();
+
+    const [stateIncome, setSateIncome] = useState(
+        {
+            entryType : "INCOME",
+            timeOfCreation : null,
+            description : null,
+            incomeSource : null,
+            preTaxAmount : 0,
+            postTaxAmount : 0
+        }
+    );
+
+    const handleFormSubmit = (event)=>{
+        event.preventDefault();
+
+        let timeOfCreationProperty = "timeOfCreation";
+
+        let copiedIncome = {...stateIncome};
+        copiedIncome[timeOfCreationProperty] = new Date();
+
+        
+    };
+
+    const handleChange = (event)=>{
+        let propertyName = event.target.name;
+
+        let copiedIncome = {...stateIncome};
+        copiedIncome[propertyName] = event.target.value;
+
+        setSateIncome(copiedIncome);
+    };
 
     const generateOptions = ()=>{
         return listOfIncomeSources.map((incomeSource)=>{
-            return <option  id={incomeSource.id} value={incomeSource}>{incomeSource.name}</option>
+            return <option  key={incomeSource.id} value={incomeSource.id}>{incomeSource.name}</option>
         });
     };
 
     return ( 
-        <form>
+        <form id="income-form" onSubmit={handleFormSubmit}>
             <label htmlFor="income-sources-select">Choose a source of income</label>
-            <select name="income-sources" id="income-sources-select">
-                <option value="" disabled selected>--Please choose and option--</option>
+            <select name="incomeSource" id="income-sources-select" onChange={handleChange}>
+                <option key="disabled-selected-income-sources" value="" disabled selected>--Please choose and option--</option>
                 {generateOptions()}
             </select>
             incomeSource
