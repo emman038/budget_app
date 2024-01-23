24e5e1057a5513adbe91bcec462d42b8535512ae
Emmanuel Ajayi <emmanuelajayi380@yahoo.co.uk>
2024-01-23 23:57:33 +0000

    created logic to generate checkboxes for the year component

--- budget_app_frontend/frontend_code/src/components/dashboardRouteComponents/timeFrameComponents/Year.js
+++ budget_app_frontend/frontend_code/src/components/dashboardRouteComponents/timeFrameComponents/Year.js
@@ -1,9 +1,37 @@
-const Year = (listOfEntries , setCurrentTimeFrame , setDashboardEntries) => {
-    return ( 
-        <form>
-            
-        </form>
-     );
+import { useState } from "react";
+
+const Year = ({listOfEntries, setCurrentTimeFrame, setDashboardEntries}) => {
+
+    const [checkedState, setCheckedState] = useState([]);
+
+    const handleChange = (index) => {
+        let copiedState = [...checkedState ];
+        copiedState[index] = !copiedState[index];
+
+        setCheckedState(copiedState);
+    };
+
+    const generateYears = () => {
+        return listOfEntries.map((entries, index) => {
+            checkedState.push(false);
+            return (
+                <label key={index}>
+                    {entries.year}
+                    <input type="checkbox" checked={checkedState[index]} onChange={() => { handleChange(index) }} />
+                </label>
+            );
+        });
+    };
+
+    return (
+        <>
+            {
+                <form>
+                    {generateYears()}
+                </form>
+            }
+        </>
+    );
 }
- 
+
 export default Year;
